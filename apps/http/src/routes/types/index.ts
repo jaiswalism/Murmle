3a777d7cf7be80a6fd5ad5522721ee42f332360d
Shyam Jaiswal <sj1464@srmist.edu.in>
2025-09-18 02:16:50 +0530

    feat(api): add Zod schemas for incoming request validation

--- apps/http/src/routes/types/index.ts
+++ apps/http/src/routes/types/index.ts
@@ -11,3 +11,50 @@ export const SigninSchema = z.object({
     password: z.string()
 })
 
+export const UpdateMetaverseSchmea = z.object({
+    avatarId: z.string()
+})
+
+export const CreateSpaceSchema = z.object({
+    name: z.string(),
+    dimensions: z.string().regex(/^[0-9]{1-4}x[0-9]{1-4}$/),
+    mapId: z.string()
+})
+
+export const AddElementSchema = z.object({
+    elementId: z.string(),
+    spaceId: z.string(),
+    x: z.number(),
+    y: z.number()
+})
+
+export const DeleteElementSchema = z.object({
+    id: z.string()
+})
+
+export const CreateElementSchema = z.object({
+    imageUrl: z.string(),
+    width: z.number(),
+    height: z.number(),
+    static: z.boolean()
+})
+
+export const UpdateElementSchema = z.object({
+    imageUrl: z.string()
+})
+
+export const CreateAvatarSchema = z.object({
+    imageUrl: z.string(),
+    name: z.string().max(15)
+})
+
+export const CreateMapSchema = z.object({
+    thumbnail: z.string(),
+    dimensions: z.string().regex(/^[0-9]{1-4}x[0-9]{1-4}$/),
+    name: z.string(),
+    defaultElements: z.array(z.object({
+        elementId: z.string(),
+        x: z.number(),
+        y: z.number()
+    }))
+})
